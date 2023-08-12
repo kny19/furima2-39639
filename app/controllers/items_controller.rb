2889c6a0eabb0760d994c0a146a96a5d66d8c01c
kny19 <ksx-ny.19@au.com>
2023-08-12 14:44:09 +0900

    商品情報編集機能完了！

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]  # ログイン状態のユーザーのみ new と create にアクセスできる
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,6 +20,26 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+
+  def edit
+    @item = Item.find(params[:id])
+    if user_signed_in?
+      redirect_to root_path unless current_user == @item.user
+    else
+      redirect_to new_user_session_path # ログインしていない場合はログインページにリダイレクト
+    end
+  end
+
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "商品情報が更新されました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
